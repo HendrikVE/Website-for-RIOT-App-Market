fe1f0a6516ee67215b99b780b36588aaae1c9705
HendrikVE <hendrik1995@msn.com>
2017-08-16 19:54:08 +0200

    building webpage with modules from database

---
+++ db_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+db_config = {
+	"host"   : "localhost",
+	"user"   : "backend_script",
+	"passwd" : "password",
+	"db"     : "riot_os"
+}
--- index.py
+++ index.py
@@ -3,30 +3,80 @@
 
 # enable debugging
 import cgitb
-cgitb.enable()
+import db_config as config
+import MySQLdb
 
-print "Content-Type: text/html"
-print "\n\r"
+def main():
+	
+	cgitb.enable()
 
-print "<!DOCTYPE html>"
-print "<html>"
-print "<head>"
-print "<title>RIOT OS App Market</title>"
-print "<link rel=""stylesheet"" href=""styles.css"">"
-print "<!-- Origin Trial Token, feature = WebUSB (For Chrome M57+), origin = https://www.vanappsteer.de, expires = 2017-08-23 -->"
-print "<meta http-equiv=""origin-trial"" data-feature=""WebUSB (For Chrome M57+)"" data-expires=""2017-08-23"" content=""AuMsLNMamUdtrJmHYTJ+0qNNi3rEKeAfbeQcVT8ZvAcdz9QJHhHk9l4kHIAPaHsMEkOGLGBYsfDOCL8BBkocHgEAAABTeyJvcmlnaW4iOiJodHRwczovL3d3dy52YW5hcHBzdGVlci5kZTo0NDMiLCJmZWF0dXJlIjoiV2ViVVNCMiIsImV4cGlyeSI6MTUwMzUxNzQxMX0="">"
-print "</head>"
-print
-print "<body>"
+	print "Content-Type: text/html"
+	print "\n\r"
 
-print "<button type=""button"" id=""selectButton"" onclick=""selectDevice()"">Select Device</button>"
+	print "<!DOCTYPE html>"
+	print "<html>"
+	print "<head>"
+	print "<title>RIOT OS App Market</title>"
+	print "<link rel=""stylesheet"" href=""styles.css"">"
+	print "<!-- Origin Trial Token, feature = WebUSB (For Chrome M57+), origin = https://www.vanappsteer.de, expires = 2017-08-23 -->"
+	print "<meta http-equiv=""origin-trial"" data-feature=""WebUSB (For Chrome M57+)"" data-expires=""2017-08-23"" content=""AuMsLNMamUdtrJmHYTJ+0qNNi3rEKeAfbeQcVT8ZvAcdz9QJHhHk9l4kHIAPaHsMEkOGLGBYsfDOCL8BBkocHgEAAABTeyJvcmlnaW4iOiJodHRwczovL3d3dy52YW5hcHBzdGVlci5kZTo0NDMiLCJmZWF0dXJlIjoiV2ViVVNCMiIsImV4cGlyeSI6MTUwMzUxNzQxMX0="">"
+	print "</head>"
+	print
+	print "<body>"
 
-print "<p><div id=""deviceInfo"" style=""white-space: pre""></div></p>"
+	print "<button type=""button"" id=""selectButton"" onclick=""selectDevice()"">Select Device</button>"
 
-print "<div id=""downloadSection"" style=""visibility: hidden;"">"
-print "<form><p>Select an application:</p><fieldset><input type=""radio"" id=""r1"" name=""component"" value=""bindist"" checked=""checked""><label for=""r1""> bindist</label><br> <input type=""radio"" id=""r2"" name=""component"" value=""gnrc_minimal""><label for=""r2""> gnrc_minimal</label><br> <input type=""radio"" id=""r3"" name=""component"" value=""timer_periodic_wakeup""><label for=""r3""> timer_periodic_wakeup</label></fieldset></form>"
+	print "<p><div id=""deviceInfo"" style=""white-space: pre""></div></p>"
 
-print "<br><button type=""button"" id=""downloadButton"" onclick=""download()"">Download your personal RIOT OS</button></div><script src=""main.js""></script>"
+	print "<div id=""downloadSection"" style=""visibility: hidden;"">"
+	
+	print_checkboxes()
 
-print "</body>"
-print "</html>"
+	print "<br><button type=""button"" id=""downloadButton"" onclick=""download()"">Download your personal RIOT OS</button></div><script src=""main.js""></script>"
+
+	print "</body>"
+	print "</html>"
+
+def print_checkboxes():
+	
+	db = MySQLdb.connect(config.db_config["host"], config.db_config["user"], config.db_config["passwd"], config.db_config["db"])
+
+	# cursor object to execute queries
+	db_cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+
+	db_cursor.execute("SELECT * FROM modules ORDER BY group_identifier")
+	results = db_cursor.fetchall()
+	
+	string_to_fill = "<li><label><input type=""checkbox"" name=""{!s}"" value=""{!s}"">{!s}</label></li>"
+
+	print "<form><h3>Select modules:</h3><fieldset>"
+	
+	last_group_identifier = None
+	group_left_open = False
+	
+	for row in results:
+		
+		group_identifier_changed = last_group_identifier != row["group_identifier"]
+		if last_group_identifier == None or group_identifier_changed:
+			
+			if group_left_open:
+				print "</ul>"
+			
+			# open new group
+			print "<p>" + row["group_identifier"] + "</p><ul>"
+			group_left_open = True
+			
+		last_group_identifier = row["group_identifier"]
+		
+		print string_to_fill.format(row["group_identifier"], row["id"], row["name"])
+
+	if group_left_open:
+		print "</ul>"
+		
+	print "</fieldset></form>"
+		
+	db_cursor.close()
+	db.close()
+	
+if __name__ == '__main__':
+	main()
