391d19f12dad961273f98a34d21a451b400d0144
HendrikVE <hendrik1995@msn.com>
2017-09-07 23:19:09 +0200

    dont offer config files to user

---
+++ config/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import cgitb, cgi
+
+cgitb.enable()
+
+print 'Content-Type: text/html'
+print '\n\r'
+
+print '<!DOCTYPE html>'
+print '<html>'
+
+print "<p>you better dont see the config files</p>"
+
+print '<html>'
