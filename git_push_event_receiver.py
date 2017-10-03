6670ecae064c98ee6e225ecfbc6af909d02d0545
HendrikVE <hendrik1995@msn.com>
2017-10-04 01:17:11 +0200

    added dummy git webhook handler

---
+++ git_push_event_receiver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+
+import random
+import logging
+import cgitb
+
+cgitb.enable()
+
+print ("Status: 403 Forbidden")
+print ("\n\r")
+
+logging.basicConfig(filename="log/index_log.txt", format="%(asctime)s [%(levelname)s]: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
+
+logging.debug(random.randint(9999))
