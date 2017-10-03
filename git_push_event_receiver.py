ba985320be9fd406609283d8c65b7cf3f6082eac
HendrikVE <hendrik1995@msn.com>
2017-10-04 01:24:41 +0200

    fixed random

--- git_push_event_receiver.py
+++ git_push_event_receiver.py
@@ -15,4 +15,4 @@ print ("\n\r")
 logging.basicConfig(filename="log/git_push_event_receiver_log.txt", format="%(asctime)s [%(levelname)s]: %(message)s",
                         datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
 
-logging.debug(random.randint(9999))
+logging.debug(random.randint(0, 9999))
