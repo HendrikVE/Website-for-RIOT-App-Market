a1bb03954172ea33bb8d3630bdb85ff091f8720b
HendrikVE <hendrik1995@msn.com>
2017-10-04 01:39:41 +0200

    dont return 403

--- git_push_event_receiver.py
+++ git_push_event_receiver.py
@@ -5,14 +5,35 @@ from __future__ import print_function
 
 import random
 import logging
-import cgitb
 
-cgitb.enable()
 
-print ("Status: 403 Forbidden")
-print ("\n\r")
+def main():
 
-logging.basicConfig(filename="log/git_push_event_receiver_log.txt", format="%(asctime)s [%(levelname)s]: %(message)s",
+    print_result("git event handling successfull")
+
+    logging.debug(random.randint(0, 9999))
+
+
+def print_result(result):
+
+    print ("Content-Type: text/html")
+    print ("\n\r")
+    print (result)
+
+
+def print_error():
+
+    print ("Status: 403 Forbidden")
+    print ("\n\r")
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(filename="log/git_push_event_receiver_log.txt", format="%(asctime)s [%(levelname)s]: %(message)s",
                         datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
 
-logging.debug(random.randint(0, 9999))
+    try:
+        main()
+
+    except Exception as e:
+        logging.error(str(e), exc_info=True)
