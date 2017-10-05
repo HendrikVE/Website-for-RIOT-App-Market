6b8e00d1a76e37bbd892d02285b02cae9f2a2fa2
HendrikVE <hendrik1995@msn.com>
2017-10-06 00:19:07 +0200

    reading in name of webhook origin repository and compare it to make a differance in tasks

--- git_push_event_receiver.py
+++ git_push_event_receiver.py
@@ -5,35 +5,79 @@ from __future__ import print_function
 
 import hashlib
 import hmac
-import random
 import logging
-
-import os
-import cgi
-
+import json
 import sys
+from os import subprocess, environ
 
-cgi.print_arguments()
-cgi.print_environ_usage()
+
+#enum
+class PushedRepo:
+    Website, Backend = range(2)
 
 
 def main():
 
-    print_result("git event handling successfull")
-
-    logging.debug(str(random.randint(0, 9999)) + ": " + str(os.environ["HTTP_X_HUB_SIGNATURE"]))
-
     secret_key = "riotam"
     request_body = sys.stdin.read()
+    is_valid = is_valid_signature(environ["HTTP_X_HUB_SIGNATURE"], secret_key, request_body)
 
-    is_valid = is_valid_signature(os.environ["HTTP_X_HUB_SIGNATURE"], secret_key, request_body)
-    logging.debug("is valid: " + str(is_valid))
+    json_message = json.loads(request_body)
+    repo_name = json_message["repository"]["full_name"]
+
+    if is_valid:
+        repo_to_update = get_repo_type(repo_name)
+
+        if repo_to_update == PushedRepo.Website:
+            update_website()
+
+        elif repo_to_update == PushedRepo.Backend:
+            update_backend()
+
+        else:
+            print_error()
+            logging.error("script called by unconfigured repository")
+            return
+
+        print_result("webhook handled successfully")
+
+    else:
+        print_error()
+
+
+def execute_command(cmd):
+
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return process.communicate()[0]
+
+
+def update_website():
+    logging.debug("update website")
+    pass
+
+
+def update_backend():
+    logging.debug("update backend")
+    pass
+
+
+def get_repo_type(name):
+
+    if name.endswith("riotam-website"):
+        return PushedRepo.Website
+
+    elif name.endswith("riotam-backend"):
+        return PushedRepo.Backend
+
+    else:
+        return None
 
 
 def is_valid_signature(signature, secret_key, body):
 
+    #https://developer.github.com/v3/repos/hooks/#create-a-hook
     #https://stackoverflow.com/questions/28228392/failed-to-verify-github-x-hub-signature-in-my-application
-    computed_signature = 'sha1=' + hmac.new(secret_key, body, hashlib.sha1).hexdigest()
+    computed_signature = "sha1=" + hmac.new(secret_key, body, hashlib.sha1).hexdigest()
 
     return computed_signature == signature
 
