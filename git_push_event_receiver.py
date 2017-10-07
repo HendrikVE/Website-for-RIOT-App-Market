b19221dc81fec9b42e1514fca6ff07e2c8f335da
HendrikVE <hendrik1995@msn.com>
2017-10-07 14:37:33 +0200

    fixed copy&paste wrong output

--- git_push_event_receiver.py
+++ git_push_event_receiver.py
@@ -76,10 +76,10 @@ def update_backend():
 
     """SETUP DATABASE"""
     output = execute_command(["python", "db_create.py", "--user", "root", "--password", "eBXa1zG2jlfwt1P71Udy"], os.path.join(PATH_RIOTAM_BACKEND, "setup"))
-    logging.debug("DB_UPDATE:\n" + output)
+    logging.debug("DB_CREATE:\n" + output)
 
     output = execute_command(["python", "db_setup.py"], os.path.join(PATH_RIOTAM_BACKEND, "setup"))
-    logging.debug("DB_UPDATE:\n" + output)
+    logging.debug("DB_SETUP:\n" + output)
 
     """UPDATE DATABASE"""
     output = execute_command(["python", "db_update.py"], PATH_RIOTAM_BACKEND)
