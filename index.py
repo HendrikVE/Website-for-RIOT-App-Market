8c4188e85430a39818845b132e12a07d9689dbb1
HendrikVE <hendrik1995@msn.com>
2017-10-08 21:07:27 +0200

    added favicon

--- index.py
+++ index.py
@@ -59,6 +59,8 @@ def html_header():
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
+            
+            <link rel="icon" href="/img/favicon.png" type="image/png">
 
             <link rel="stylesheet" href="/css/bootstrap.min.css">
             <link rel="stylesheet" type="text/css" href="/css/custom.css" />
