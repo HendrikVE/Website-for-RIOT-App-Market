41fe08dbae9270db5c8dcf1f0e533776bd4d4b35
HendrikVE <hendrik1995@msn.com>
2017-12-19 12:47:46 +0100

    checking for success in do_precheck

--- js/main.js
+++ js/main.js
@@ -182,9 +182,9 @@ function download() {
     chrome.runtime.sendMessage(chromeExtensionId, {request: "native_messaging_host_accessible"},
         function(response) {
 
-            do_prechecks();
-
-            download_post();
+            if (do_prechecks()) {
+                download_post();
+            }
         }
     );
 }
@@ -309,33 +309,37 @@ function download_example(applicationID, progressDivID, progressBarID, panelID, 
     chrome.runtime.sendMessage(chromeExtensionId, {request: "native_messaging_host_accessible"},
         function(response) {
 
-            do_prechecks();
-
-            download_example_post(applicationID, progressDivID, progressBarID, panelID, buttonID, modalDialogID);
+            if (do_prechecks()) {
+                download_example_post(applicationID, progressDivID, progressBarID, panelID, buttonID, modalDialogID);
+            }
         }
     );
 }
 
+// return true if everything went fine, false in case of failure
 function do_prechecks() {
+
     //first check: is the extension itself installed/ activated
     if (chrome.runtime.lastError) {
         if (chrome.runtime.lastError.message == "Could not establish connection. Receiving end does not exist.") {
             alert("You need to install the RIOT OS AppMarket Extension. See https://github.com/HendrikVE/riotam-chrome-integration");
-            return;
+            return false;
         }
     }
 
     //second check: look in to the response if the extension was able to connect to native messaging host
     if(!response.success) {
         alert("You need to install the riotam Native Messaging Host provided in riotam-chrome-integration/native-messaging-host/");
-        return;
+        return false;
     }
 
     //third check: is another download already running?
     if(downloadIsRunning) {
         alert("Another process is already running, please wait until it is finished.");
-        return;
+        return false;
     }
+
+    return true;
 }
 
 
